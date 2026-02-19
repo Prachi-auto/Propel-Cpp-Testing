f9af4d1a74a12cdfae60cacad1de278536e8e85b
Prachi-auto <86566845+Prachi-auto@users.noreply.github.com>
2026-02-19 11:40:34 +0530

    Addressing review comments

--- ADAS/RelevantObjectsCommon.hpp
+++ ADAS/RelevantObjectsCommon.hpp
@@ -13,20 +13,23 @@ public:
     /// @brief Constructor for RelevantObjectsCommon.
     /// @param max_longitudinal_range_meters Maximum longitudinal range in meters to consider an object relevant.
     explicit RelevantObjectsCommon(float max_longitudinal_range_meters);
+    /// @brief Destructor for RelevantObjectsCommon.
+    virtual ~RelevantObjectsCommon() = default;
 
-    /// @brief Chooses the AEB target object from the detected set.  
+    /// @brief Chooses the nearest relevant object in the ego lane. 
     /// @param all_objects Detected objects.
     /// @param ego_lane Ego Lane Index.
     /// @return The nearest relevant object in the ego lane, or a empty optional
     std::optional<Object> getTargetObject(const ObjectList& all_objects, const LaneId ego_lane) const;
 
-private:
+protected:
     /// @brief returns if the given object is relevant for aeb.
     /// @param object object to check if it's relevant in ego lane
     /// @param ego_lane Ego Lane Index.
     /// @return true if object is relevant for aeb else false.
     bool isRelevant(const Object& object, const LaneId ego_lane) const;
 
+private:
     /// @brief Maximum longitudinal range in meters to consider an object relevant.
     float m_max_longitudinal_range_meters;
 };
