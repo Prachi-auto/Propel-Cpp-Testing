bdc4757fc566ab29807dbfd7fce16acadafbeba2
Prachi-auto <86566845+Prachi-auto@users.noreply.github.com>
2026-02-22 12:57:29 +0530

    increase the object list size to 6

---
+++ ADAS/ObjectBuilder.hpp
@@ -0,0 +1,52 @@
+#ifndef OBJECT_BUILDER_HPP
+#define OBJECT_BUILDER_HPP
+
+#include "ADAS/Object.hpp"
+
+class ObjectBuilder
+{
+public:
+	ObjectBuilder() = default;
+
+	ObjectBuilder& withId(ObjectId id)
+	{
+		m_object.m_id = id;
+		return *this;
+	}
+
+	ObjectBuilder& withPosition(float x, float y = 0.0f)
+	{
+		m_object.m_position.m_x = x;
+		m_object.m_position.m_y = y;
+		return *this;
+	}
+
+	ObjectBuilder& withVelocity(float x = 0.0f, float y = 0.0f)
+	{
+		m_object.m_velocity.m_x = x;
+		m_object.m_velocity.m_y = y;
+		return *this;
+	}
+
+	ObjectBuilder& withAcceleration(float x = 0.0f, float y = 0.0f)
+	{
+		m_object.m_acceleration.m_x = x;
+		m_object.m_acceleration.m_y = y;
+		return *this;
+	}
+
+	ObjectBuilder& inLane(LaneId lane)
+	{
+		m_object.m_lane = lane;
+		return *this;
+	}
+
+	Object build() const { return m_object; }
+
+private:
+	Object m_object{};
+};
+
+
+
+#endif
