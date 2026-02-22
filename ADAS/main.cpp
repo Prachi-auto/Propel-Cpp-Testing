62b8fc4c659bb1609f004260a2ef1ce95ff17bd2
Prachi-auto <86566845+Prachi-auto@users.noreply.github.com>
2026-02-22 12:57:29 +0530

    increase the object list size to 6

--- ADAS/main.cpp
+++ ADAS/main.cpp
@@ -18,13 +18,14 @@ void CreateObject(Object& object, ObjectId id, float x_position, LaneId lane)
 
 ObjectList CreateObjects()
 {
-    ObjectList objects(5);
+    ObjectList objects(6);
 
     CreateObject(objects[0], 1, 10.0f, LaneId::Center);
     CreateObject(objects[1], 2, 20.0f, LaneId::Left);
     CreateObject(objects[2], 3, 30.0f, LaneId::Right);
     CreateObject(objects[3], 4, 40.0f, LaneId::Center);
     CreateObject(objects[4], 5, 60.0f, LaneId::Center);
+    CreateObject(objects[5], 6, 70.0f, LaneId::Right);
 
     return objects;
 }
