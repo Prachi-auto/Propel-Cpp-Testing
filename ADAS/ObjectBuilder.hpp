641afe0b3fc9ce2555cccf5c41a0622ac14277a0
Prachi-auto <86566845+Prachi-auto@users.noreply.github.com>
2026-02-22 19:02:37 +0530

    inconsistent tabs spaces

--- ADAS/ObjectBuilder.hpp
+++ ADAS/ObjectBuilder.hpp
@@ -6,47 +6,45 @@
 class ObjectBuilder
 {
 public:
-	ObjectBuilder() = default;
+    ObjectBuilder() = default;
 
-	ObjectBuilder& withId(ObjectId id)
-	{
-		m_object.m_id = id;
-		return *this;
-	}
+    ObjectBuilder& withId(ObjectId id)
+    {
+        m_object.m_id = id;
+        return *this;
+    }
 
-	ObjectBuilder& withPosition(float x, float y = 0.0f)
-	{
-		m_object.m_position.m_x = x;
-		m_object.m_position.m_y = y;
-		return *this;
-	}
+    ObjectBuilder& withPosition(float x, float y = 0.0f)
+    {
+        m_object.m_position.m_x = x;
+        m_object.m_position.m_y = y;
+        return *this;
+    }
 
-	ObjectBuilder& withVelocity(float x = 0.0f, float y = 0.0f)
-	{
-		m_object.m_velocity.m_x = x;
-		m_object.m_velocity.m_y = y;
-		return *this;
-	}
+    ObjectBuilder& withVelocity(float x = 0.0f, float y = 0.0f)
+    {
+        m_object.m_velocity.m_x = x;
+        m_object.m_velocity.m_y = y;
+        return *this;
+    }
 
-	ObjectBuilder& withAcceleration(float x = 0.0f, float y = 0.0f)
-	{
-		m_object.m_acceleration.m_x = x;
-		m_object.m_acceleration.m_y = y;
-		return *this;
-	}
+    ObjectBuilder& withAcceleration(float x = 0.0f, float y = 0.0f)
+    {
+        m_object.m_acceleration.m_x = x;
+        m_object.m_acceleration.m_y = y;
+        return *this;
+    }
 
-	ObjectBuilder& inLane(LaneId lane)
-	{
-		m_object.m_lane = lane;
-		return *this;
-	}
+    ObjectBuilder& inLane(LaneId lane)
+    {
+        m_object.m_lane = lane;
+        return *this;
+    }
 
-	Object build() const { return m_object; }
+    Object build() const { return m_object; }
 
 private:
-	Object m_object{};
+    Object m_object{};
 };
-
-
 
 #endif
