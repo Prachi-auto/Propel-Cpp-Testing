c3b9c5fb734e628cadce48a80236887abd78cb85
Prachi-auto <86566845+Prachi-auto@users.noreply.github.com>
2026-02-25 12:02:05 +0530

    Create objects function to return object

--- ADAS/main.cpp
+++ ADAS/main.cpp
@@ -5,8 +5,9 @@
 #include "ADAS/Constants.hpp"
 #include <iostream>
 
-void CreateObject(Object& object, ObjectId id, float x_position, LaneId lane)
+Object CreateObject(ObjectId id, float x_position, LaneId lane)
 {
+    Object object{};
     object.m_id = id;
     object.m_position.m_x = x_position;
     object.m_position.m_y = 0.0f;
@@ -15,18 +16,19 @@ void CreateObject(Object& object, ObjectId id, float x_position, LaneId lane)
     object.m_acceleration.m_x = 0.0f;
     object.m_acceleration.m_y = 0.0f;
     object.m_lane = lane;
+    return object;
 }
 
 ObjectList CreateObjects()
 {
     ObjectList objects(ObjectSelectionConstants::m_number_of_objects);
 
-    CreateObject(objects[0], 1, 10.0f, LaneId::Center);
-    CreateObject(objects[1], 2, 20.0f, LaneId::Left);
-    CreateObject(objects[2], 3, 30.0f, LaneId::Right);
-    CreateObject(objects[3], 4, 40.0f, LaneId::Center);
-    CreateObject(objects[4], 5, 60.0f, LaneId::Center);
-    CreateObject(objects[5], 6, 70.0f, LaneId::Right);
+    objects[0] = CreateObject(1, 10.0f, LaneId::Center);
+    objects[1] = CreateObject(2, 20.0f, LaneId::Left);
+    objects[2] = CreateObject(3, 30.0f, LaneId::Right);
+    objects[3] = CreateObject(4, 40.0f, LaneId::Center);
+    objects[4] = CreateObject(5, 60.0f, LaneId::Center);
+    objects[5] = CreateObject(6, 70.0f, LaneId::Right);
 
     return objects;
 }
